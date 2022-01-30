4e04b6c5f89f64894c043c01ffaa9375e07a7f8f
yash-03 <yksrivastava1@gmail.com>
2022-01-30 17:23:32 +0530

    set cors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
-const port = 4000;
+const PORT = 4000;
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("welcome to blog api");
 });
 
-app.listen(port, () => {
-  console.log(`server listen on ${port}`);
+app.listen(PORT, () => {
+  console.log(`server listen on ${PORT}`);
 });
