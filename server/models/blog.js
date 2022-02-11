2dbc2a2e95f2ec10210157592deb658f2d7d2a27
yash-03 <yksrivastava1@gmail.com>
2022-02-11 23:39:06 +0530

    create blog schema

---
+++ server/models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    comments: [{ body: String, date: Date }],
+    meta: {
+      votes: Number,
+      favs: Number,
+    },
+    active: { type: Boolean, default: true },
+    createdBy: String,
+    createdAt: { type: Date, default: new Date() },
+    updatedAt: { type: Date, default: new Date() },
+  },
+  { versionKey: false }
+);
+
+const blogModel = model("Users", blogSchema);
+
+module.exports = blogModel;
